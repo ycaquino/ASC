adf30fb4fa5b40006b2687b4243f59523fcf7bc5
Yan Carlos Aquino Gutierrez <yan.aquino1@upr.edu>
2021-01-07 15:45:26 -0400

    Added products page

---
+++ JS/products.js
@@ -0,0 +1,9 @@
+jQuery.get('/text/products-p1.txt', function(data) {
+    //First paragraph of products page page 
+    $('#products-p1').html(data.replace('n',''));
+ });
+
+ jQuery.get('/text/products-p2.txt', function(data) {
+  //Second paragraph of products page 
+  $('#products-p2').html(data.replace('n',''));
+});
