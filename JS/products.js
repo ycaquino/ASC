b268decca4f2ec983d3a785f2419b7caab84ded8
Yan Carlos Aquino Gutierrez <yan.aquino1@upr.edu>
2021-01-13 16:32:41 -0400

    Refactored code

--- JS/products.js
+++ JS/products.js
@@ -1,9 +1,9 @@
 jQuery.get('/Text/products-p1.txt', function(data) {
     //First paragraph of products page page 
-    $('#products-p1').html(data.replace('n',''));
+    $('#products-p1').html(data.replace('',''));
  });
 
  jQuery.get('/Text/products-p2.txt', function(data) {
   //Second paragraph of products page 
-  $('#products-p2').html(data.replace('n',''));
+  $('#products-p2').html(data.replace('',''));
 });
